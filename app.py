ccc7a02497969950c6d0041bf5f5724214e011de
ming <chenming@guanmai.cn>
2019-12-29 17:59:47 +0800

    修改运行ip

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from exts.cloudflask import CloudFlask
 app = CloudFlask.create_app(config_dot_path='config.DevConfig')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
